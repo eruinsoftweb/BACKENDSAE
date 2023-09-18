5b31c9fed388e84f0a295acf4667447c381c3edf
cesaracjota <cesar.acjota@tecsup.edu.pe>
2023-09-18 06:46:00 -0500

    first commit

--- helpers/menu-frontend.js
+++ helpers/menu-frontend.js
@@ -1,112 +1,255 @@
 const getMenuFrontEnd = (role, modalidad) => {
-
-    const menu =  [
+    const menu = [
         {
             titulo: 'Inicio',
-            icono : 'RiHome5Fill',
+            icono: 'RiHome5Fill',
             path: '/'
-        },
-        {
-            titulo: 'Usuarios',
-            icono : 'FaUsers',
-            path : '/usuarios'
-        },
-        {
-            titulo: 'Grados',
-            icono : 'MdGrade',
-            path : '/grados'
         },
     ];
 
-    switch (role, modalidad) {
-        case 'ADMIN_ROLE', 'CEBA':
-            menu.push(
-                {
-                    titulo: 'Estudiantes',
-                    icono : 'RiUserStarFill',
-                    path : '/ceba/estudiantes'
-                },
-                {
-                    titulo: 'Pagos',
-                    icono : 'MdMonetizationOn',
-                    path : '/ceba/pagos'
-                },
-                {
-                    titulo: 'Reportes',
-                    icono : 'FaChartPie',
-                    path : '/ceba/reportes'
-                })
+    if (role === 'ADMIN_ROLE') {
+        switch (modalidad) {
+            case 'CEBA':
+                menu.push(
+                    {
+                        titulo: 'Usuarios',
+                        icono: 'FaUsers',
+                        path: '/usuarios'
+                    },
+
+                    {
+                        titulo: 'Grados',
+                        icono: 'MdGrade',
+                        path: '/grados'
+                    },
+                    {
+                        titulo: 'Estudiantes',
+                        icono: 'RiUserStarFill',
+                        path: '/ceba/estudiantes'
+                    },
+                    {
+                        titulo: 'Pagos',
+                        icono: 'MdMonetizationOn',
+                        path: '/ceba/pagos'
+                    },
+                    {
+                        titulo: 'Reportes',
+                        icono: 'FaChartPie',
+                        path: '/ceba/reportes'
+                    }
+                );
                 break;
-        case 'ADMIN_ROLE', 'RESIDENCIA':
-            menu.push(
-                {
-                    titulo: 'Estudiantes',
-                    icono : 'RiUserStarFill',
-                    path : '/residencia/estudiantes'
-                }, {
-                    titulo: 'Pagos',
-                    icono : 'MdMonetizationOn',
-                    path : '/residencia/pagos'
-                },
-                {
-                    titulo: 'Reportes',
-                    icono : 'FaChartPie',
-                    path : '/residencia/reportes'
-                })
+            case 'RESIDENCIA':
+                menu.push(
+                    {
+                        titulo: 'Usuarios',
+                        icono: 'FaUsers',
+                        path: '/usuarios'
+                    },
+
+                    {
+                        titulo: 'Grados',
+                        icono: 'MdGrade',
+                        path: '/grados'
+                    },
+                    {
+                        titulo: 'Estudiantes',
+                        icono: 'RiUserStarFill',
+                        path: '/residencia/estudiantes'
+                    },
+                    {
+                        titulo: 'Pagos',
+                        icono: 'MdMonetizationOn',
+                        path: '/residencia/pagos'
+                    },
+                    {
+                        titulo: 'Reportes',
+                        icono: 'FaChartPie',
+                        path: '/residencia/reportes'
+                    }
+                );
                 break;
-        case 'ADMIN_ROLE', 'EBR':
-            menu.push(
-                {
-                    titulo: 'Estudiantes',
-                    icono : 'RiUserStarFill',
-                    path : '/ebr/estudiantes'
-                },
-                {
-                    titulo: 'Pagos',
-                    icono : 'MdMonetizationOn',
-                    path : '/ebr/pagos'
-                },
-                {
-                    titulo: 'Docentes',
-                    icono : 'FaChalkboardTeacher',
-                    path : '/ebr/docentes'
-                },
-                {
-                    titulo: 'Equipos',
-                    icono : 'RiComputerFill',
-                    path : '/ebr/equipos'
-                },
-                {
-                    titulo: 'Libros',
-                    icono : 'RiBook3Fill',
-                    path : '/ebr/libros'
-                },
-                {
-                    titulo: 'Inmobiliarios',
-                    icono : 'MdTableChart',
-                    path : '/ebr/inmobiliarios'
-                },
-                {
-                    titulo: 'Uniformes',
-                    icono : 'FaVest',
-                    path : '/ebr/uniformes'
-                },
-                {
-                    titulo: 'Mapas',
-                    icono : 'RiMapPin4Fill',
-                    path : '/ebr/mapas'
-                },
-                {
-                    titulo: 'Laboratorios',
-                    icono : 'MdScience',
-                    path : '/ebr/laboratorios'
-                },
-                {
-                    titulo: 'Reportes',
-                    icono : 'FaChartPie',
-                    path : '/ebr/reportes'
-                })
+            case 'EBR':
+                menu.push(
+                    {
+                        titulo: 'Usuarios',
+                        icono: 'FaUsers',
+                        path: '/usuarios'
+                    },
+
+                    {
+                        titulo: 'Grados',
+                        icono: 'MdGrade',
+                        path: '/grados'
+                    },
+                    {
+                        titulo: 'Estudiantes',
+                        icono: 'RiUserStarFill',
+                        path: '/ebr/estudiantes'
+                    },
+                    {
+                        titulo: 'Pagos',
+                        icono: 'MdMonetizationOn',
+                        path: '/ebr/pagos'
+                    },
+                    {
+                        titulo: 'Docentes',
+                        icono: 'FaChalkboardTeacher',
+                        path: '/ebr/docentes'
+                    },
+                    {
+                        titulo: 'Equipos',
+                        icono: 'RiComputerFill',
+                        path: '/ebr/equipos'
+                    },
+                    {
+                        titulo: 'Libros',
+                        icono: 'RiBook3Fill',
+                        path: '/ebr/libros'
+                    },
+                    {
+                        titulo: 'Inmobiliarios',
+                        icono: 'MdTableChart',
+                        path: '/ebr/inmobiliarios'
+                    },
+                    {
+                        titulo: 'Uniformes',
+                        icono: 'FaVest',
+                        path: '/ebr/uniformes'
+                    },
+                    {
+                        titulo: 'Mapas',
+                        icono: 'RiMapPin4Fill',
+                        path: '/ebr/mapas'
+                    },
+                    {
+                        titulo: 'Laboratorios',
+                        icono: 'MdScience',
+                        path: '/ebr/laboratorios'
+                    },
+                    {
+                        titulo: 'Reportes',
+                        icono: 'FaChartPie',
+                        path: '/ebr/reportes'
+                    }
+                );
                 break;
+        }
+    }
+
+    if (role === 'USER_ROLE') {
+        switch (modalidad) {
+            case 'CEBA':
+                menu.push(
+                    {
+                        titulo: 'Estudiantes',
+                        icono: 'RiUserStarFill',
+                        path: '/ceba/estudiantes'
+                    },
+
+                    {
+                        titulo: 'Grados',
+                        icono: 'MdGrade',
+                        path: '/grados'
+                    },
+                    {
+                        titulo: 'Pagos',
+                        icono: 'MdMonetizationOn',
+                        path: '/ceba/pagos'
+                    },
+                    {
+                        titulo: 'Reportes',
+                        icono: 'FaChartPie',
+                        path: '/ceba/reportes'
+                    }
+                );
+                break;
+            case 'RESIDENCIA':
+                menu.push(
+
+                    {
+                        titulo: 'Grados',
+                        icono: 'MdGrade',
+                        path: '/grados'
+                    },
+                    {
+                        titulo: 'Estudiantes',
+                        icono: 'RiUserStarFill',
+                        path: '/residencia/estudiantes'
+                    },
+                    {
+                        titulo: 'Pagos',
+                        icono: 'MdMonetizationOn',
+                        path: '/residencia/pagos'
+                    },
+                    {
+                        titulo: 'Reportes',
+                        icono: 'FaChartPie',
+                        path: '/residencia/reportes'
+                    }
+                );
+                break;
+            case 'EBR':
+                menu.push(
+
+                    {
+                        titulo: 'Grados',
+                        icono: 'MdGrade',
+                        path: '/grados'
+                    },
+                    {
+                        titulo: 'Estudiantes',
+                        icono: 'RiUserStarFill',
+                        path: '/ebr/estudiantes'
+                    },
+                    {
+                        titulo: 'Pagos',
+                        icono: 'MdMonetizationOn',
+                        path: '/ebr/pagos'
+                    },
+                    {
+                        titulo: 'Docentes',
+                        icono: 'FaChalkboardTeacher',
+                        path: '/ebr/docentes'
+                    },
+                    {
+                        titulo: 'Equipos',
+                        icono: 'RiComputerFill',
+                        path: '/ebr/equipos'
+                    },
+                    {
+                        titulo: 'Libros',
+                        icono: 'RiBook3Fill',
+                        path: '/ebr/libros'
+                    },
+                    {
+                        titulo: 'Inmobiliarios',
+                        icono: 'MdTableChart',
+                        path: '/ebr/inmobiliarios'
+                    },
+                    {
+                        titulo: 'Uniformes',
+                        icono: 'FaVest',
+                        path: '/ebr/uniformes'
+                    },
+                    {
+                        titulo: 'Mapas',
+                        icono: 'RiMapPin4Fill',
+                        path: '/ebr/mapas'
+                    },
+                    {
+                        titulo: 'Laboratorios',
+                        icono: 'MdScience',
+                        path: '/ebr/laboratorios'
+                    },
+                    {
+                        titulo: 'Reportes',
+                        icono: 'FaChartPie',
+                        path: '/ebr/reportes'
+                    }
+                );
+                break;
+        }
     }
 
     return menu;
@@ -115,3 +258,4 @@ const getMenuFrontEnd = (role, modalidad) => {
 module.exports = {
     getMenuFrontEnd
 }
+
